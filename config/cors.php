45340e03ec2dd36c2e225aadd5226cf00af206e7
Gustavo Ulyssea <gustavo.ulyssea@gmail.com>
2025-06-26 14:57:23 -0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://sipresc.gum.net.br'
+    ],
 
     'allowed_origins_patterns' => [],
 
